76c0d0eeacdd9bcc8b5db66e706299888f3bb7c8
ariel.pineiro <ariel.pineiro@iskaypet.com>
2023-04-14 17:14:58 +0200

    example

---
+++ example/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"go.dp.iskaypet.com/tools/dev/go-restclient/rest"
+)
+
+type UserDTO struct {
+	ID   int64
+	Name string
+}
+
+func main() {
+	requestBuilder := rest.RequestBuilder{
+		Timeout:        time.Millisecond * 3000,
+		ConnectTimeout: time.Millisecond * 5000,
+		BaseURL:        "https://gorest.co.in/public/v2",
+	}
+
+	// This won't be blocked.
+	requestBuilder.AsyncGet("/users", func(response *rest.Response) {
+		if response.StatusCode == http.StatusOK {
+			log.Println(response)
+		}
+	})
+
+	response := requestBuilder.Get("/users")
+	if response.StatusCode != http.StatusOK {
+		log.Fatal(response.Err.Error())
+	}
+
+	var usersDto []UserDTO
+	err := response.FillUp(&usersDto)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// or typed filled up
+	_, err = rest.TypedFillUp[[]UserDTO](response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var futures []*rest.FutureResponse
+
+	requestBuilder.ForkJoin(func(c *rest.Concurrent) {
+		for i := 0; i < len(usersDto); i++ {
+			futures = append(futures, c.Get(fmt.Sprintf("/users/%d", usersDto[i].ID)))
+		}
+	})
+
+	log.Println("Wait all ...")
+	startTime := time.Now()
+	for i := range futures {
+		if futures[i].Response().StatusCode == http.StatusOK {
+			var userDto UserDTO
+			convertionErr := futures[i].Response().FillUp(&userDto)
+			if convertionErr != nil {
+				log.Fatal(convertionErr)
+			}
+			log.Println("\t" + userDto.Name)
+		}
+	}
+	elapsedTime := time.Since(startTime)
+	log.Printf("Elapsed time: %d", elapsedTime)
+}
