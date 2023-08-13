8f3ee65e331fdf80cd21a01ca2f9406efafe0da5
ariel.pineiro <ariel.pineiro@iskaypet.com>
2023-08-13 10:06:28 -0300

    changes

--- .gitlab/common/git.sh
+++ .gitlab/common/git.sh
@@ -2,7 +2,7 @@
 cat <<EOF >>"$HOME"/.netrc
 machine gitlab.com
   login master_token
-  password $CICD_TOKEN
+  password $GITLAB_TOKEN
 EOF
 
-git config --global url."https://oauth2:${CICD_TOKEN}@gitlab.com".insteadOf "https://gitlab.com"
+git config --global url."https://oauth2:${GITLAB_TOKEN}@gitlab.com".insteadOf "https://gitlab.com"
