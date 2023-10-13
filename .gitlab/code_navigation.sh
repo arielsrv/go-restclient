970bc344ef196b1f575003b900f86ca2e201bd6a
ariel.pineiro <ariel.pineiro@iskaypet.com>
2023-10-13 13:13:35 +0200

    lsif

--- .gitlab/code_navigation.sh
+++ .gitlab/code_navigation.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 .gitlab/common/git.sh
+curl -L  https://github.com/sourcegraph/lsif-go/releases/download/v1.2.0/src_linux_amd64 -o /usr/local/bin/lsif-go
+chmod +x /usr/local/bin/lsif-go
 lsif-go
