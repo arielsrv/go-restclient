a3ed287dcd8c3415129d1a7ae8a3d1f091ee3d0e
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-01-05 10:08:25 +0100

    fix

--- .gitlab/code_navigation.sh
+++ .gitlab/code_navigation.sh
@@ -5,6 +5,6 @@ TAG=v1.9.3
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m | sed -e 's/x86_64/amd64/')
 echo "${TAG}" "${OS}" "${ARCH}"
-curl https://github.com/sourcegraph/lsif-go/releases/download/"${TAG}"/src_"${OS}"_"${ARCH}" -o /usr/local/bin/lsif-go
+curl -L https://github.com/sourcegraph/lsif-go/releases/download/"${TAG}"/src_"${OS}"_"${ARCH}" -o /usr/local/bin/lsif-go
 chmod +x /usr/local/bin/lsif-go
 lsif-go
