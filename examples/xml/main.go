09d47e42e03bebbd99cde365681036a4b0ae5d26
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-11-14 17:52:40 +0100

    fix

--- examples/xml/main.go
+++ examples/xml/main.go
@@ -10,25 +10,23 @@ import (
 	"gitlab.com/iskaypetcom/digital/sre/tools/dev/go-restclient/rest"
 )
 
-type UserResponse struct {
-	Name   string `xml:"name"`
-	Email  string `xml:"email"`
-	Gender string `xml:"gender"`
-	Status string `xml:"status"`
-	ID     int    `xml:"id"`
-}
-
-type UsersResponse struct {
-	XMLName string         `xml:"objects"`
-	List    []UserResponse `xml:"object"`
-}
-
 func main() {
 	httpClient := &rest.RequestBuilder{
 		BaseURL:        "https://gorest.co.in/public/v2",
 		Timeout:        time.Millisecond * 2000,
 		ConnectTimeout: time.Millisecond * 5000,
 		ContentType:    rest.XML,
+	}
+
+	var usersResponse struct {
+		XMLName string `xml:"objects"`
+		List    []struct {
+			Name   string `xml:"name"`
+			Email  string `xml:"email"`
+			Gender string `xml:"gender"`
+			Status string `xml:"status"`
+			ID     int    `xml:"id"`
+		} `xml:"object"`
 	}
 
 	response := httpClient.GetWithContext(context.Background(), "/users.xml")
@@ -40,7 +38,7 @@ func main() {
 		log.Fatalf("Status: %d, Body: %s", response.StatusCode, response.Body)
 	}
 
-	usersResponse, err := rest.Deserialize[UsersResponse](response)
+	err := response.FillUp(&usersResponse)
 	if err != nil {
 		log.Fatal(err)
 	}
