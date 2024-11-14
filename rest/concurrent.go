969f7954e25182e9cc993f98f42edc958950442f
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-11-14 13:12:42 +0100

    concurrent

--- rest/concurrent.go
+++ rest/concurrent.go
@@ -9,6 +9,24 @@ import (
 	"unsafe"
 )
 
+type IConcurrent interface {
+	Get(url string) *FutureResponse
+	Post(url string, body interface{}) *FutureResponse
+	Patch(url string, body interface{}) *FutureResponse
+	Put(url string, body interface{}) *FutureResponse
+	Delete(url string) *FutureResponse
+	Head(url string) *FutureResponse
+	Options(url string) *FutureResponse
+
+	GetWithContext(ctx context.Context, url string) *FutureResponse
+	PostWithContext(ctx context.Context, url string, body interface{}) *FutureResponse
+	PatchWithContext(ctx context.Context, url string, body interface{}) *FutureResponse
+	PutWithContext(ctx context.Context, url string, body interface{}) *FutureResponse
+	DeleteWithContext(ctx context.Context, url string) *FutureResponse
+	HeadWithContext(ctx context.Context, url string) *FutureResponse
+	OptionsWithContext(ctx context.Context, url string) *FutureResponse
+}
+
 // FutureResponse represents a response to be completed after a ForkJoin
 // operation is done.
 //
@@ -27,7 +45,7 @@ func (fr *FutureResponse) Response() *Response {
 }
 
 // Concurrent has methods for Get, Post, Put, Patch, Delete, Head & Options,
-// with the almost the same API as the synchronous methods.
+// with almost the same API as the synchronous methods.
 // The difference is that these methods return a FutureResponse, which holds a pointer to
 // Response. Response inside FutureResponse is nil until the request has finished.
 //
@@ -51,7 +69,7 @@ type Concurrent struct {
 // Client should expect a response status code of 200(OK) if resource exists,
 // 404(Not Found) if it doesn't, or 400(Bad Request).
 func (c *Concurrent) Get(url string) *FutureResponse {
-	return c.doRequest(context.Background(), http.MethodGet, url, nil)
+	return c.GetWithContext(context.Background(), url)
 }
 
 // Post issues a POST HTTP verb to the specified URL, concurrently with any other
@@ -63,7 +81,7 @@ func (c *Concurrent) Get(url string) *FutureResponse {
 //
 // Body could be any of the form: string, []byte, struct & map.
 func (c *Concurrent) Post(url string, body interface{}) *FutureResponse {
-	return c.doRequest(context.Background(), http.MethodPost, url, body)
+	return c.PostWithContext(context.Background(), url, body)
 }
 
 // Patch issues a PATCH HTTP verb to the specified URL, concurrently with any other
@@ -75,7 +93,7 @@ func (c *Concurrent) Post(url string, body interface{}) *FutureResponse {
 //
 // Body could be any of the form: string, []byte, struct & map.
 func (c *Concurrent) Patch(url string, body interface{}) *FutureResponse {
-	return c.doRequest(context.Background(), http.MethodPatch, url, body)
+	return c.PatchWithContext(context.Background(), url, body)
 }
 
 // Put issues a PUT HTTP verb to the specified URL, concurrently with any other
@@ -87,7 +105,7 @@ func (c *Concurrent) Patch(url string, body interface{}) *FutureResponse {
 //
 // Body could be any of the form: string, []byte, struct & map.
 func (c *Concurrent) Put(url string, body interface{}) *FutureResponse {
-	return c.doRequest(context.Background(), http.MethodPut, url, body)
+	return c.PutWithContext(context.Background(), url, body)
 }
 
 // Delete issues a DELETE HTTP verb to the specified URL, concurrently with any other
@@ -97,7 +115,7 @@ func (c *Concurrent) Put(url string, body interface{}) *FutureResponse {
 // Client should expect a response status code of 200(OK), 404(Not Found),
 // or 400(Bad Request).
 func (c *Concurrent) Delete(url string) *FutureResponse {
-	return c.doRequest(context.Background(), http.MethodDelete, url, nil)
+	return c.DeleteWithContext(context.Background(), url)
 }
 
 // Head issues a HEAD HTTP verb to the specified URL, concurrently with any other
@@ -107,7 +125,7 @@ func (c *Concurrent) Delete(url string) *FutureResponse {
 // Client should expect a response status code of 200(OK) if resource exists,
 // 404(Not Found) if it doesn't, or 400(Bad Request).
 func (c *Concurrent) Head(url string) *FutureResponse {
-	return c.doRequest(context.Background(), http.MethodHead, url, nil)
+	return c.HeadWithContext(context.Background(), url)
 }
 
 // Options issues a OPTIONS HTTP verb to the specified URL, concurrently with any other
@@ -118,7 +136,84 @@ func (c *Concurrent) Head(url string) *FutureResponse {
 // Client should expect a response status code of 200(OK) if resource exists,
 // 404(Not Found) if it doesn't, or 400(Bad Request).
 func (c *Concurrent) Options(url string) *FutureResponse {
-	return c.doRequest(context.Background(), http.MethodOptions, url, nil)
+	return c.OptionsWithContext(context.Background(), url)
+}
+
+// GetWithContext issues a GET HTTP verb to the specified URL, concurrently with any other
+// concurrent requests that may be called.
+//
+// In Restful, GET is used for "reading" or retrieving a resource.
+// Client should expect a response status code of 200(OK) if resource exists,
+// 404(Not Found) if it doesn't, or 400(Bad Request).
+func (c *Concurrent) GetWithContext(ctx context.Context, url string) *FutureResponse {
+	return c.doRequest(ctx, http.MethodGet, url, nil)
+}
+
+// PostWithContext issues a POST HTTP verb to the specified URL, concurrently with any other
+// concurrent requests that may be called.
+//
+// In Restful, POST is used for "creating" a resource.
+// Client should expect a response status code of 201(Created), 400(Bad Request),
+// 404(Not Found), or 409(Conflict) if resource already exist.
+//
+// Body could be any of the form: string, []byte, struct & map.
+func (c *Concurrent) PostWithContext(ctx context.Context, url string, body interface{}) *FutureResponse {
+	return c.doRequest(ctx, http.MethodPost, url, body)
+}
+
+// PatchWithContext issues a PATCH HTTP verb to the specified URL, concurrently with any other
+// concurrent requests that may be called.
+//
+// In Restful, PATCH is used for "partially updating" a resource.
+// Client should expect a response status code of 200(OK), 404(Not Found),
+// or 400(Bad Request). 200(OK) could be also 204(No Content)
+//
+// Body could be any of the form: string, []byte, struct & map.
+func (c *Concurrent) PatchWithContext(ctx context.Context, url string, body interface{}) *FutureResponse {
+	return c.doRequest(ctx, http.MethodPatch, url, body)
+}
+
+// PutWithContext issues a PUT HTTP verb to the specified URL, concurrently with any other
+// concurrent requests that may be called.
+//
+// In Restful, PUT is used for "updating" a resource.
+// Client should expect a response status code of 200(OK), 404(Not Found),
+// or 400(Bad Request). 200(OK) could be also 204(No Content)
+//
+// Body could be any of the form: string, []byte, struct & map.
+func (c *Concurrent) PutWithContext(ctx context.Context, url string, body interface{}) *FutureResponse {
+	return c.doRequest(ctx, http.MethodPut, url, body)
+}
+
+// DeleteWithContext issues a DELETE HTTP verb to the specified URL, concurrently with any other
+// concurrent requests that may be called.
+//
+// In Restful, DELETE is used to "delete" a resource.
+// Client should expect a response status code of 200(OK), 404(Not Found),
+// or 400(Bad Request).
+func (c *Concurrent) DeleteWithContext(ctx context.Context, url string) *FutureResponse {
+	return c.doRequest(ctx, http.MethodDelete, url, nil)
+}
+
+// HeadWithContext issues a HEAD HTTP verb to the specified URL, concurrently with any other
+// concurrent requests that may be called.
+//
+// In Restful, HEAD is used to "read" a resource headers only.
+// Client should expect a response status code of 200(OK) if resource exists,
+// 404(Not Found) if it doesn't, or 400(Bad Request).
+func (c *Concurrent) HeadWithContext(ctx context.Context, url string) *FutureResponse {
+	return c.doRequest(ctx, http.MethodHead, url, nil)
+}
+
+// OptionsWithContext issues a OPTIONS HTTP verb to the specified URL, concurrently with any other
+// concurrent requests that may be called.
+//
+// In Restful, OPTIONS is used to get information about the resource
+// and supported HTTP verbs.
+// Client should expect a response status code of 200(OK) if resource exists,
+// 404(Not Found) if it doesn't, or 400(Bad Request).
+func (c *Concurrent) OptionsWithContext(ctx context.Context, url string) *FutureResponse {
+	return c.doRequest(ctx, http.MethodOptions, url, nil)
 }
 
 func (c *Concurrent) doRequest(ctx context.Context, verb string, url string, reqBody interface{}) *FutureResponse {
