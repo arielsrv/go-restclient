5bf1a5ddfbf9ca7e558e760cd8a296a1bdc8b5ee
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-12-02 09:02:22 +0100

    probability

--- rest/net.go
+++ rest/net.go
@@ -216,21 +216,21 @@ func (r *Client) shouldCompress(request *http.Request, response *http.Response) 
 
 // setContentReader creates a reader from the given body and content type.
 func setContentReader(body any, contentType ContentType) (io.Reader, error) {
-	if body != nil {
-		mediaContent, found := contentMarshalers[contentType]
-		if !found {
-			return nil, fmt.Errorf("marshal fail, unsupported content type: %d", contentType)
-		}
-
-		reader, err := mediaContent.Marshal(body)
-		if err != nil {
-			return nil, err
-		}
-
-		return reader, nil
+	if body == nil {
+		return http.NoBody, nil
 	}
 
-	return http.NoBody, nil
+	mediaContent, found := contentMarshalers[contentType]
+	if !found {
+		return nil, fmt.Errorf("marshal fail, unsupported content type: %d", contentType)
+	}
+
+	reader, err := mediaContent.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return reader, nil
 }
 
 // setRespReader creates a reader from the given request and response.
