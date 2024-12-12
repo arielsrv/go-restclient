a7b91b702695472ec66bdc587c4485d25633545f
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-12-13 00:11:33 +0100

    examples

--- examples/html/main.go
+++ examples/html/main.go
@@ -7,25 +7,33 @@ import (
 	"time"
 
 	"gitlab.com/iskaypetcom/digital/sre/tools/dev/go-logger/log"
-
 	"gitlab.com/iskaypetcom/digital/sre/tools/dev/go-restclient/rest"
 )
 
 func main() {
-	httpClient := &rest.Client{
-		BaseURL:        "https://syndicate.synthrone.com",
-		Timeout:        time.Millisecond * 2000,
-		ConnectTimeout: time.Millisecond * 5000,
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
+
+	client := &rest.Client{
+		Name:       "html-client",
+		EnableGzip: true,
+		Timeout:    100 * time.Millisecond, ConnectTimeout: 100 * time.Millisecond,
 	}
 
-	response := httpClient.GetWithContext(context.Background(), "/df9g5m2kxcv7/ROY153637_M/latest/ROY153637_M.html")
-	if response.Err != nil {
+	fmt.Printf("[go-restclient] example started, MaxCacheSize: %d, NumCounters: %f, BufferItems: %d\n",
+		rest.MaxCacheSize,
+		rest.NumCounters,
+		rest.BufferItems,
+	)
+
+	response := client.GetWithContext(ctx, "https://syndicate.synthrone.com/df9g5m2kxcv7/ROY153637_M/latest/ROY153637_M.html")
+
+	switch {
+	case response.Err != nil:
 		log.Fatal(response.Err)
+	case response.StatusCode != http.StatusOK:
+		log.Fatalf("status_code: %d, reason: %s", response.StatusCode, response.String())
+	default:
+		fmt.Printf("%s\n", response.String())
 	}
-
-	if response.StatusCode != http.StatusOK {
-		log.Fatalf("Status: %d, Body: %s", response.StatusCode, response.String())
-	}
-
-	fmt.Printf("Response: %s", response.String())
 }
